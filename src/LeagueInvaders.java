408091a3d92426124d8313fe0526182847c4b1ef
student <student@ip-172-31-58-112.us-west-2.compute.internal>
2023-02-21 18:26:02 -0800

    6:25 PM 2/21/23

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -2,18 +2,26 @@ import javax.swing.JFrame;
 
 public class LeagueInvaders {
 	JFrame frame = new JFrame();
+	GamePanel panel = new GamePanel();
+	
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-	
-	public LeagueInvaders(JFrame frame) {
-		this.frame = frame;
+
+	LeagueInvaders() {
+		this.frame = new JFrame();
+		this.panel = new GamePanel();
 	}
-	
+
 	void setup() {
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
 	}
-	
+
 	public static void main(String[] args) {
-		//LeagueInvaders newFrame = new LeagueInvaders(frame);
+		LeagueInvaders newFrame = new LeagueInvaders();
+		newFrame.setup();
 	}
 }
