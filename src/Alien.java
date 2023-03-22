c7dad213c66473e72b245ad792596794c8791cd4
student <student@ip-172-31-53-83.us-west-2.compute.internal>
2023-03-21 18:21:25 -0700

    6:21 PM 3/21/23

--- src/Alien.java
+++ src/Alien.java
@@ -2,10 +2,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Alien extends GameObject {
-
-	public Alien(int x, int y, int width, int height, int speed) {
-		super(x, y, width, height, speed);
-		speed = 1;
+	public Alien(int x, int y, int width, int height) {
+		super(x, y, width, height, 1);
 		// TODO Auto-generated constructor stub
 	}
 
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,3 +1,38 @@
-public class ObjectManager {
-Rocketship rooket;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ObjectManager{
+
+	Rocketship rocket;
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+	Random random = new Random();
+
+	public ObjectManager(Rocketship rocket) {
+		this.rocket = rocket;
+	}
+
+	void addProjectile(Projectile p) {
+		projectiles.add(p);
+	}
+
+	void addAlien() {
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+	}
+
+	void update() {
+		for(Alien alien : aliens) {
+			alien.update();
+			if(alien.y < 0 || alien.y > LeagueInvaders.HEIGHT) {
+				alien.isActive = false;
+			}
+		}
+		for(Projectile proj : projectiles) {
+			proj.update();
+			if(proj.y < 0 || proj.y > LeagueInvaders.HEIGHT) {
+				proj.isActive = false;
+			}
+					
+		}
+	}
 }
